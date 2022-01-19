452753aaba3074fffc964e75d13afc22114500f9
Navruz Rakhimov <rakhimovnavruz@gmail.com>
2022-01-19 09:22:25 +0500

    part-4-testing

--- backend/app/db/repositories/cleanings.py
+++ backend/app/db/repositories/cleanings.py
@@ -8,6 +8,12 @@ CREATE_CLEANING_QUERY = """
     RETURNING id, name, description, price, cleaning_type;
 """
 
+GET_CLEANING_BY_ID_QUERY = """
+    SELECT id, name, description, price, cleaning_type
+    FROM cleanings
+    WHERE id = :id;
+"""
+
 
 class CleaningsRepository(BaseRepository):
     """
@@ -18,5 +24,14 @@ class CleaningsRepository(BaseRepository):
         query_values = new_cleaning.dict()
         cleaning = await self.db.fetch_one(query=CREATE_CLEANING_QUERY, values=query_values)
 
+        return CleaningInDB(**cleaning)
+
+    
+    async def get_cleaning_by_id(self, *, id: int) -> CleaningInDB:
+        cleaning = await self.db.fetch_one(query=GET_CLEANING_BY_ID_QUERY, values={"id": id})
+        
+        if not cleaning:
+            return None
+        
         return CleaningInDB(**cleaning)
 
